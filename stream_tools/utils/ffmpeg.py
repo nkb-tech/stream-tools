129f98c4d085f7375665f7d6df2be465c3ef3ae9
ilyabasharov <ilya.basharov.98@mail.ru>
2024-05-07 13:59:15 +0300

    fix

--- stream_tools/utils/ffmpeg.py
+++ stream_tools/utils/ffmpeg.py
@@ -1,11 +1,12 @@
 from torchaudio.utils import ffmpeg_utils
 
+
 def make_ffmpeg_decoder(decoder: str, gpu: bool) -> str:
     '''Check and make ffmpeg decoder
     Inputs:
         decoder: str
-        device: bool if true than gpu, false - cpu 
-    
+        device: bool if true than gpu, false - cpu
+
     Returns:
     str
     '''
@@ -15,5 +16,5 @@ def make_ffmpeg_decoder(decoder: str, gpu: bool) -> str:
 
     assert decoder in ffmpeg_utils.get_video_decoders(), \
         f'Decoder {decoder} is not supported in FFmpeg. Please check available decoder.'
-    
+
     return decoder
